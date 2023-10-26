d96fb7de5007d4dc5b509e42a8d288b941846bcc
r4cs <r.guzansky@hotmail.com>
2023-10-26 17:57:54 -0300

    openApi swagger implementado. Precisa 'personalizar'

--- src/main/java/br/com/challenge/procurement/config/SwaggerConfig.java
+++ src/main/java/br/com/challenge/procurement/config/SwaggerConfig.java
@@ -1,43 +1,24 @@
 package br.com.challenge.procurement.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.service.VendorExtension;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import java.util.ArrayList;
-//
-//import static springfox.documentation.builders.PathSelectors.regex;
-//
-//@Configuration
-//@EnableSwagger2
-public class SwaggerConfig {}
-//
-//    @Bean
-//    public Docket procurementApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("br.com.challenge.procurement.apiController"))
-//                .paths(regex("/api/.*"))
-//                .build()
-//                .apiInfo(metaInfo());
-//    }
-//
-//    private ApiInfo metaInfo() {
-//        ApiInfo apiInfo = new ApiInfo(
-//                "Procurement API REST",
-//                "Conecta informações relacionadas as etapas do procurement e retorna em uma API",
-//                "0.1",
-//                "Terms of Service",
-//                new Contact("Raquel Guzansky", "https://github.com/r4cs", "r.guzansky@hotmail.com"),
-//                "Apache License Version 2.0",
-//                "https://www.apache.org/licensen.html", new ArrayList<VendorExtension>()
-//        );
-//        return apiInfo;
-//    }
-//}
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI geraDocumentacao() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080/api");
+        devServer.description("Url de desenvolvimento local");
+        Contact contact = new Contact();
+        contact.setEmail("rm97373@fiap.com.br");
+        contact.setName("Raquel");
+        Info info = new Info().title("Checkpoint 1 e 2")
+                .version("0.1")
+                .contact(contact)
+                .description("Projeto referente a procurement");
+        return new OpenAPI().info(info).servers(List.of(devServer));  }}
