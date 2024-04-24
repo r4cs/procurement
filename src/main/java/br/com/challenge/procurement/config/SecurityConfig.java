d7c2a13164f5e81ce35292fc43c8eb31d7601a79
r4cs <r.guzansky@hotmail.com>
2024-04-24 01:10:30 -0300

    inclusao de sprint security, templates, configs

---
+++ src/main/java/br/com/challenge/procurement/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package br.com.challenge.procurement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(_cors ->
+                        _cors.configurationSource(request -> {
+                                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                                    corsConfiguration.addAllowedOrigin("*");
+                                    corsConfiguration.addAllowedHeader("*");
+                                    corsConfiguration.addAllowedMethod("*");
+                                    return corsConfiguration;
+                        })
+                )
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/login").permitAll()
+                                .requestMatchers(
+                                        "/logout",
+                                        "/api/**").authenticated()
+//                                        "/api/swagger/**",
+//                                        "/api/swagger-ui/**",
+//                                        "/configuration/ui",
+//                                        "/swagger-resources/**",
+//                                        "/configuration/security",
+//                                        "/webjars/**",
+//                                        "/v3/**").authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .permitAll()
+                                .defaultSuccessUrl("/api/swagger-ui/index.html")
+                ).logout((logout) -> logout
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/v3/api-docs/**", "api/swagger.json/**", "/swagger-resources/**"
+        );
+    }
+}
