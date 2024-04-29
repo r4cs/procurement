a1ab5240467037429a6c43d1af19eca0f2b8ad7e
r4cs <r.guzansky@hotmail.com>
2024-04-29 13:29:43 -0300

    adicionando logs no oauth controller

--- src/main/java/br/com/challenge/procurement/core/authController/AuthController.java
+++ src/main/java/br/com/challenge/procurement/core/authController/AuthController.java
@@ -6,27 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class AuthController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @GetMapping("/")
     String index(Principal principal) {
-        System.out.println("Principal em root /: " + principal);
+        logger.debug("Principal em root /: {}", principal);
         return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
     }
 
     @GetMapping("/login")
     String login() {
+        logger.debug("Acessando a página de login");
         return "/home/homeNotSignedIn";
     }
 
     @GetMapping("/logout")
     String logout() {
+        logger.debug("Acessando a página de logout");
         return "/home/homeNotSignedIn";
     }
 
     @GetMapping("/login/github")
     public String loginWithGithub(HttpServletRequest request) {
+        logger.debug("Redirecionando para login do GitHub");
         String redirectUri = "https://app-procurement.azurewebsites.net/.auth/login/github/callback";
         String clientId = "dea1fe6183f99a004c90";
         String githubLoginUrl = "https://github.com/login/oauth2/authorize" +
