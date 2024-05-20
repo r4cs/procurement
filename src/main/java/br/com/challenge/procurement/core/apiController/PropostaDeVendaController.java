7514766b685a86d5fc7e449004c0223f6a52dc37
r4cs <r.guzansky@hotmail.com>
2024-05-20 13:42:22 -0300

    endpoint de proposta adequado para tables

--- src/main/java/br/com/challenge/procurement/core/apiController/PropostaDeVendaController.java
+++ src/main/java/br/com/challenge/procurement/core/apiController/PropostaDeVendaController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/proposta-fornecedor")
+@RequestMapping("/api/proposta")
 public class PropostaDeVendaController {
 
     private final PropostaDeVendaService service;
